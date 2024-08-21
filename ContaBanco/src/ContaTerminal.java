f1d837632370b2545a3dc07e92ec0e8ef0d1c515
emaildoissa@gmail.com <emaildoissa@gmail.com>
2024-08-21 16:50:55 -0300

    Meu projeto Conta Banco_Entrega

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String saldo = NumberFormat.getCurrencyInstance().format(5000.00);
+        
+        Scanner scanner = new Scanner(System.in);
+        //
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite o nome da agencia: ");
+        int agencia = scanner.nextInt();
+
+        System.out.print("Digite sua conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Olá "+ nome + ", obrigado por criar uma conta em nosso banco, ");
+        System.out.println("sua agência é "+ agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
+       
+
     }
 }
